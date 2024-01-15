ea1c6123fb25529dceb333fe7fef9cfd7ffb0efa
IASIKO <iaseshviligi@gmail.com>
2024-01-15 13:41:45 +0400

    add last body section component

---
+++ src/components/Body sections/CtaEnd.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from '../../css/components/Body sections/CtaEnd.module.css'
+
+const CtaEnd = () => {
+  return (
+    <section className={styles.ctaEnd}>
+      <h2>Boost your links today</h2>
+      <a href="#">Get Started</a>
+    </section>
+  );
+};
+
+export default CtaEnd;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,17 +2,15 @@ import styles from "../css/components/Body.module.css";
 import Cta from "./Body sections/Cta";
 import UrlShortening from "./Body sections/UrlShortening";
 import Services from "./Body sections/Services";
+import CtaEnd from "./Body sections/CtaEnd";
 
 const Body = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <Cta />
       <UrlShortening />
       <Services />
-      <section className={styles.ctaEnd}>
-        <h2>Boost your links today</h2>
-        <a href="#">Get Started</a>
-      </section>
+      <CtaEnd />
     </main>
   );
 };
