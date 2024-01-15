7a308586803a1c3b81ec85ca76c6d8dc55a0e26f
IASIKO <iaseshviligi@gmail.com>
2024-01-15 13:37:33 +0400

    add third body section

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,81 +1,14 @@
 import styles from "../css/components/Body.module.css";
-import recognition from "../assets/icon-brand-recognition.svg";
-import detail from "../assets/icon-detailed-records.svg";
-import fully from "../assets/icon-fully-customizable.svg";
-import { useShortening } from "../hooks/useShortening";
 import Cta from "./Body sections/Cta";
 import UrlShortening from "./Body sections/UrlShortening";
+import Services from "./Body sections/Services";
 
 const Body = () => {
-  const { links, shortenLinks, onCopyHandler, copiedIndex } = useShortening();
-
   return (
-    <main className={styles.main}>
+    <main>
       <Cta />
       <UrlShortening />
-      <section className={styles.servicies}>
-        <div className={styles.urlDisplay}>
-          <ul>
-            {shortenLinks.map((link, i) => (
-              <li className={styles.urlList} key={i}>
-                <span className={styles.currentUrl}>{links[i]}</span>
-                <div className={styles.shortUrlBox}>
-                  <span className={styles.shortUrl}>{link}</span>
-                  <button
-                    onClick={() => onCopyHandler(link, i)}
-                    className={`${styles.copyButton} ${
-                      copiedIndex === i && styles.copiedButton
-                    }`}
-                  >
-                    {copiedIndex === i ? "Copied!" : "Copy"}
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.serviceTitle}>
-          <h2>Advanced Statistics</h2>
-          <p>
-            Track how your links are performing across the web with our advanced
-            statistics dashboard.
-          </p>
-        </div>
-        <div className={styles.serviceBoxes}>
-          <div className={styles.boxOne}>
-            <div>
-              <img src={recognition} alt="brand recognition" />
-            </div>
-            <p className={styles.oneTitle}>Brand Recognition</p>
-            <p className={styles.oneText}>
-              Boost your brand recognition with each click. Generic links don't
-              mean a thing. Branded links help instil confidence in your
-              content.
-            </p>
-          </div>
-          <div className={styles.boxTwo}>
-            <div>
-              <img src={detail} alt="detailed records" />
-            </div>
-            <p className={styles.oneTitle}>Detailed Records</p>
-            <p className={styles.oneText}>
-              Gain insights into who is clicking your links. Knowing when and
-              where people engage with your content helps inform better
-              decisions.
-            </p>
-          </div>
-          <div className={styles.boxThree}>
-            <div>
-              <img src={fully} alt="fully customizable" />
-            </div>
-            <p className={styles.oneTitle}>Fully Customizable</p>
-            <p className={styles.oneText}>
-              Improve brand awareness and content discoverability through
-              customizable links, supercharging audience engagement.
-            </p>
-          </div>
-        </div>
-      </section>
+      <Services />
       <section className={styles.ctaEnd}>
         <h2>Boost your links today</h2>
         <a href="#">Get Started</a>
